2a98a4edd3fd57a753b1f11d965bdb1efd32ae72
RobertCardoso <robert.rcardoso@hotmail.com>
2022-07-28 20:00:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
+  const [listArticles, searchForm] = useState([])
+
+  fetchData(){
+     fetch('http://hn.algolia.com/api/v1/items/%27')
+    .then(response => response.json())
+    .then(data => {searchForm(data.results)})
+    console.log(listArticles)
+  
+  }
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
